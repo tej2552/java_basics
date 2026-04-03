c88c246cbfdb5dd7473b02367a774a5c4e86522e
TEJAS <tejasc@testvagrant.com>
2026-04-03 12:24:26 +0530

    ArrayList vs LinkedList

--- src/main/java/Concepts/Lambdas/Lambdas.java
+++ src/main/java/Concepts/Lambdas/Lambdas.java
@@ -9,6 +9,6 @@ public class Lambdas {
     public static void main(String[] args) {
         Lambdas lambdas = new Lambdas();
         lambdas.printThing(() -> System.out.println("YAY I printed using lambdas"));
+        lambdas.printThing(() -> System.out.println("This is another implementation of my lambda"));
     }
-
 }
