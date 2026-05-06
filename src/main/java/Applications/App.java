fd88fde5953174c5e720da1e4acd9b9dbc4ea4e8
TEJAS <tejasc@testvagrant.com>
2026-05-06 18:00:32 +0530

    created an elevator simulation using stacks

---
+++ src/main/java/Applications/App.java
@@ -0,0 +1,65 @@
+package Applications;
+
+import java.util.Stack;
+
+public class App {
+
+    Stack<Integer> personStack = new Stack<>();
+    Stack<Integer> personWithMaxPriority = new Stack<>();
+    Stack<Integer> personWithLowPriority = new Stack<>();
+
+    public void addPerson(int priority){
+        personStack.push(priority);
+
+        if(personWithMaxPriority.isEmpty() && personWithLowPriority.isEmpty()){
+            personWithMaxPriority.push(priority);
+            personWithLowPriority.push(priority);
+        }else{
+            if(priority >= personWithMaxPriority.peek()){
+                personWithMaxPriority.push(priority);
+            }
+
+            if(priority <= personWithLowPriority.peek()){
+                personWithLowPriority.push(priority);
+            }
+        }
+    }
+
+    public int dropLastPersonEntered(){
+        int val = personStack.pop();
+
+        if(val == personWithMaxPriority.peek()){
+            personWithMaxPriority.pop();
+        }
+
+        if(val == personWithLowPriority.peek()){
+            personWithLowPriority.pop();
+        }
+
+        return val;
+    }
+
+    public int showPersonNearToDoor(){
+        return personStack.peek();
+    }
+
+    public int showPersonWithHighestPriority(){
+        return personWithMaxPriority.peek();
+    }
+
+    public int showPersonWithLeastPriority(){
+        return personWithLowPriority.peek();
+    }
+
+    public void showAllPersonPriority(){
+        Stack<Integer> copy = new Stack<>();
+        copy.addAll(personStack);
+
+        System.out.println("___________________________");
+        System.out.println("ELEVATOR INFO");
+        while (!copy.isEmpty()){
+            System.out.println("\t"+copy.pop());
+        }
+        System.out.println("___________________________");
+    }
+}
---
+++ src/main/java/Applications/Main.java
@@ -0,0 +1,58 @@
+package Applications;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        App app = new App();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("_________________________________________");
+            System.out.println("Hello! Lets play elevator elevation");
+            System.out.println("What do u wanna do");
+            System.out.println("1. Add a person into elevator");
+            System.out.println("2. Show the person with highest priority");
+            System.out.println("3. Show the person with least priority");
+            System.out.println("4. Show who is closest to the elevator door");
+            System.out.println("5. Drop out the person who entered last");
+            System.out.println("6. Exit");
+            System.out.println("_________________________________________");
+
+            int choice = sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.println("Tell me the priority of the person");
+                    int pr = sc.nextInt();
+                    app.addPerson(pr);
+                    app.showAllPersonPriority();
+                    System.out.println("Current highest prioritised person in elevator is -> "+app.showPersonWithHighestPriority());
+                    break;
+                case 2:
+                    app.showAllPersonPriority();
+                    System.out.println("Highest priority of a person in the elevator currently is - > "+app.showPersonWithHighestPriority());
+                    break;
+                case 3:
+                    app.showAllPersonPriority();
+                    System.out.println("Least priority of a person in the elevator currently is - > "+app.showPersonWithLeastPriority());
+                    break;
+                case 4:
+                    app.showAllPersonPriority();
+                    System.out.println("Person with priority "+app.showPersonNearToDoor()+" is nearest to door");
+                    break;
+                case 5:
+                    System.out.println("Dropping the person with priority "+app.dropLastPersonEntered());
+                    app.showAllPersonPriority();
+                    System.out.println("Current highest prioritised person in elevator is -> "+app.showPersonWithHighestPriority());
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("Ok thanks for your time");
+                    break;
+            }
+        }
+    }
+}
